2caf831693e33c8725249186f8ebe319a35a0f38
duce <dptwink@gmail.com>
2021-11-23 18:37:23 +0300

    Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class HelloWorld {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("Hello World!");
     }
 }
