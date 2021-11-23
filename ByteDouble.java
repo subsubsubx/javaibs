7df882fb0cf5a636736fa1ebfd73405795413232
duce <dptwink@gmail.com>
2021-11-23 19:02:19 +0300

    ByteDouble commit

---
+++ ByteDouble.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+public class ByteDouble {
+    public static void main(String[] args){
+        byte b = 127;
+        double d = 22.88;
+        System.out.println(b + "\n" + d);
+    }
+}
+
