50c4dd7cab3ef88029aa355a656412a5b2051ddb
duce <dptwink@gmail.com>
2021-12-09 18:39:13 +0300

    arrays v2

--- arrays.java
+++ arrays.java
@@ -1,16 +1,14 @@
 package com.company;
 
-public class Main {
-	
-    public static void main(String[] args) {
-        int[] testarray = new int[]{15, 20, 25, 30, 35};
-        int first = testarray[0];
-        int last = testarray[testarray.length - 1];
-        int mid = testarray[testarray.length - 3];
-int temp = first;
-first = last;
-last = temp;
-int res = mid + first;
-System.out.println(res);
+import java.util.Arrays;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            int[] testarray = new int[]{15, 20, 25, 30, 35};
+            int temp = testarray[0];
+            testarray[0] = testarray[testarray.length - 1];
+            testarray[testarray.length - 1] = temp;
+            System.out.println(testarray[0] + testarray[testarray.length - 3]);
+        }
     }
-}
